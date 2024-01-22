7e87af6cfc199f683a6351598ad329c6811cbbe2
Lucas Menezes <luckmenez@gmail.com>
2024-01-21 22:55:26 -0300

    feat: Filter creation

---
+++ src/pages/Home/Filter/index.tsx
@@ -0,0 +1,13 @@
+import { FilterContainer, InputQuery } from "./styles";
+
+export function Filter() {
+  return (
+    <FilterContainer>
+      <div>
+        <span>Publicações</span>
+        <span>6 visualizações</span>
+      </div>
+      <InputQuery />
+    </FilterContainer>
+  );
+}
---
+++ src/pages/Home/Filter/styles.ts
@@ -0,0 +1,29 @@
+import { styled } from "styled-components";
+
+export const FilterContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 1.33rem;
+  margin-bottom: 3rem;
+  
+  & > div {
+    display: flex;
+    justify-content: space-between;
+  }
+
+  @media (max-width: 1280px) {
+    margin: 4.5rem 1rem 4.5rem 1rem;
+  }
+`
+
+export const InputQuery = styled.input`
+  display: flex;
+  padding: 12px 16px;
+  align-items: center;
+  gap: 8px;
+
+  border-radius: 6px;
+  border: 1px solid var(--Base-Border, #1C2F41);
+  background: var(--Base-Input, #040F1A);
+`
+

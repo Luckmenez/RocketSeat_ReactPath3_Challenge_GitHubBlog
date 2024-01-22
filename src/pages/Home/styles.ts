1cae31a13abc8080913ff0f169878e9a13a39631
Lucas Menezes <luckmenez@gmail.com>
2024-01-21 23:01:02 -0300

    feat: created grid for issues card

--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -4,3 +4,15 @@ import { styled } from "styled-components";
 export const HomeContainer = styled.div`
   
 `
+
+export const IssuesGrid = styled.div`
+  width: 100%;
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  gap: 2rem;
+
+  @media (max-width: 768px) {
+    grid-template-columns: 1fr;
+  }
+`
+

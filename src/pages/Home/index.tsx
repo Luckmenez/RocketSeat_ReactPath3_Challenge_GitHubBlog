a46fba8e75d25628a0c78f7f777c2bf29caf2f81
Lucas Menezes <luckmenez@gmail.com>
2024-01-21 22:58:09 -0300

    feat: Filter and issueCard implementation

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,10 +1,20 @@
+import { IssueCard } from "../../components/IssueCard";
 import { ProfileCard } from "../../components/ProfileCard";
-import { HomeContainer } from "./styles";
+import { Filter } from "./Filter";
+import { HomeContainer, IssuesGrid } from "./styles";
+
+// const list = [{}, {}];
 
 export default function Home() {
   return (
     <HomeContainer>
       <ProfileCard />
+      <Filter />
+      <IssuesGrid>
+        <IssueCard />
+        <IssueCard />
+        <IssueCard />
+      </IssuesGrid>
     </HomeContainer>
   );
 }

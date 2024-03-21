b90adf4b530a308549d7b38ffc1483655d24ef89
Lucas Menezes <luckmenez@gmail.com>
2024-03-20 23:36:09 -0300

    feat: adding total_count to filter

--- src/pages/Home/Filter/index.tsx
+++ src/pages/Home/Filter/index.tsx
@@ -1,11 +1,15 @@
 import { FilterContainer, InputQuery } from "./styles";
 
-export function Filter() {
+interface FilterProps {
+  total_count: number;
+}
+
+export function Filter({ total_count }: FilterProps) {
   return (
     <FilterContainer>
       <div>
         <span>Publicações</span>
-        <span>6 visualizações</span>
+        <span>{total_count} visualizações</span>
       </div>
       <InputQuery />
     </FilterContainer>

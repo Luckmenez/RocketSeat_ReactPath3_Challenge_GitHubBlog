37b9bb9833f35319c00c9e5bd3d5f21f8d58fdd9
Lucas Menezes <luckmenez@gmail.com>
2024-03-24 22:52:33 -0300

    feat: issue page first implementation

---
+++ src/pages/Issue/index.tsx
@@ -0,0 +1,9 @@
+import { IssueDescriptionCard } from "../../components/IssueDescriptionCard";
+
+export default function Issue() {
+  return (
+    <div>
+      <IssueDescriptionCard />
+    </div>
+  );
+}
---
+++ src/pages/Issue/styles.ts
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+
+
+export const IssueContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+`;

a6f5b94283ca713918354ec78410866312bea076
Lucas Menezes <luckmenez@gmail.com>
2024-03-24 22:53:06 -0300

    feat: lazy and suspense route implementation

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,15 +1,19 @@
-import { lazy } from "react";
+import { lazy, Suspense } from "react";
 import { Route, Routes } from "react-router-dom";
 import { DefaultLayout } from "./layout/DefaultLayout";
 
 const Home = lazy(() => import("./pages/Home"));
+const Issue = lazy(() => import("./pages/Issue"));
 
 export function Router() {
   return (
-    <Routes>
-      <Route path="/" element={<DefaultLayout />}>
-        <Route path="/" element={<Home />} />
-      </Route>
-    </Routes>
+    <Suspense fallback={<div>Carregando...</div>}>
+      <Routes>
+        <Route path="/" element={<DefaultLayout />}>
+          <Route path="/" element={<Home />} />
+          <Route path="/issue" element={<Issue />} />
+        </Route>
+      </Routes>
+    </Suspense>
   );
 }

412aa4225c6b84f4c4d55066c05720aec64b2bc7
Lucas Menezes <luckmenez@gmail.com>
2024-03-25 22:35:36 -0300

    feat: adding issue implementation from navigation data

--- src/pages/Issue/index.tsx
+++ src/pages/Issue/index.tsx
@@ -1,9 +1,21 @@
+import { useContext } from "react";
 import { IssueDescriptionCard } from "../../components/IssueDescriptionCard";
+import { useLocation } from "react-router-dom";
+import { Issue, IssuesContext } from "../../context/issues-context";
 
 export default function Issue() {
+  const { items } = useContext(IssuesContext);
+  const location = useLocation();
+  const id = location.state.id;
+  const issue = items.find((issue) => issue.id === id);
+
+  if (!issue) {
+    return <div>Issue not found</div>;
+  }
+
   return (
     <div>
-      <IssueDescriptionCard />
+      <IssueDescriptionCard issue={issue} />
     </div>
   );
 }
